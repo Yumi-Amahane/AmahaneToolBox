799107dd7512f8e550496e64d425cb866d82b680
Yumi_Amahane <51439946+Yumi-Amahane@users.noreply.github.com>
2020-09-30 19:25:05 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="AmahaneToolBox",
+    version="0.0.0",
+    author="Yumi-Amahane",
+    description="AmahaneToolBox",
+    url="https://github.com/Yumi-Amahane/AmahaneToolBox",
+    py_modules=["AmahaneToolBox"],
+)
